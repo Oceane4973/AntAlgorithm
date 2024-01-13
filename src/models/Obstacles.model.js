019a02103a4eba16dd67556ec6a58f4dae6b97f9
emmanuel <emmanuel.lion@brl.fr>
2024-01-13 19:11:37 +0100

    create obstacles view

--- src/models/Obstacles.model.js
+++ src/models/Obstacles.model.js
@@ -2,8 +2,8 @@ import StaticElementsModel from "./StaticElementsModel.model";
 
 class ObstaclesModel extends StaticElementsModel{
 
-    constructor(map = null, positionX = 0, positionY = 0) {
-        super(map , positionX, positionY, true);
+    constructor(canva = null, positionX = 0, positionY = 0) {
+        super(canva , positionX, positionY, true);
     }
 
 }
