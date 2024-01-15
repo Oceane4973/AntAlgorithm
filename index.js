c7f96d81376c1085e52edc3ee739ac6ccfc0db9f
emmanuel <emmanuel.lion@brl.fr>
2024-01-15 16:16:09 +0100

    play/pause of game engine

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
-import controlPanel from "./src/controllers/controlPanel.controller.js";
+import ControlPanel from "./src/controllers/ControlPanel.controller.js";
 import Canvas from './src/controllers/Canvas.controller.js'
 import GameEngine from "./src/GameEngine.js";
 
-const els = [1,2,3,4];
-const engine = new GameEngine(els);
-
-let first = true;
-
-const control = new controlPanel(() => {
-    if(control.play){
-        cancelAnimationFrame(animation);
-
-    } else {
-        if(first){
-            start();
-            first = false;
-        } else
-            animation = requestAnimationFrame(start);
-    }
-});
-
 window.addEventListener('load', function() {
-    const control = new controlPanel();
+    const els = [1,2,3,4];
+
+    const engine = new GameEngine(els);
+    const control = new ControlPanel(engine);
 
     const screenHeight = window.innerHeight
     const canvasHeight = screenHeight * 0.9
@@ -29,11 +14,4 @@ window.addEventListener('load', function() {
     const canvasController = new Canvas("game", screenHeight, canvasHeight)
     canvasController.generateMatrixBackground()
     canvasController.generateBackground()
-})
-
-let animation = null;
-
-function start() {
-    engine.update();
-    animation = requestAnimationFrame(start);
-}
+});
---
+++ src/controllers/ControlPanel.controller.js
@@ -0,0 +1,43 @@
+class ControlPanel {
+
+    constructor(engine = null){
+        this.clicked = 0;
+        this.btn = document.querySelector("#control");
+        this.setupListener();
+        this.play = false;
+        this.engine = engine;
+        this.begin = false;
+        this.animation = null;
+        this.loop = this.loop.bind(this);
+    }
+
+    setupListener(){
+        this.btn.addEventListener('click', () => {
+            this.clicked += 1;
+            this.play = this.clicked % 2 === 0;
+            this.refresh();
+        })
+    }
+
+    refresh(){
+        if(this.engine !== null && this.engine !== undefined){
+            if(this.play){
+                cancelAnimationFrame(this.animation);
+            } else {
+                if(this.begin){
+                    this.loop();
+                    this.begin = false;
+                } else
+                    this.animation = requestAnimationFrame(this.loop);
+            }
+        }
+        this.btn.textContent = this.play ? "Start" : "Pause";
+    }
+
+    loop(){
+        this.engine.update();
+        this.animation = requestAnimationFrame(this.loop);
+    }
+}
+
+export default ControlPanel;
