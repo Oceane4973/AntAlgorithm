cdc2905e69b5b7b3b68a5371c9b6ccc0d39c2196
Océane MONGES <oceane@bebou.oceanemonges.com>
2024-01-23 09:42:17 +0100

    Add pheromones

--- src/models/Cell.model.js
+++ src/models/Cell.model.js
@@ -5,22 +5,42 @@ class Cell {
     constructor(type = CellType.TREE) {
         this.type = type
         this.image = ImageLoader.instance.images[type]
+        this.pheromones = (type == CellType.ANTHILL || type == CellType.FOOD) ? 1 : 0
+    }
+
+    draw_default_grass(context, x, y, cellSize){
+         const grassImg = ImageLoader.instance.images[CellType.FLOOR]
+         const grassSquareSize = grassImg.img.width / grassImg.croppedValue
+         const grassXPos = grassImg.img.width * grassImg.xRatio
+         const grassYPos = grassImg.img.width * grassImg.yRatio
+         context.drawImage( grassImg.img, grassXPos, grassYPos, grassSquareSize, grassSquareSize, x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+
+    draw_image(context, x, y, cellSize){
+        const squareSize = this.image.img.width / this.image.croppedValue
+        const xPos = this.image.img.width * this.image.xRatio
+        const yPos = this.image.img.width * this.image.yRatio
+        const objectSize = cellSize * this.image.sizeRatio
+        context.drawImage( this.image.img, xPos, yPos, squareSize, squareSize, (x * cellSize) , (y * cellSize), objectSize, objectSize);
+    }
+
+    draw_pheromone(context, x, y, cellSize){
+        context.textAlign = "center"
+        context.fillStyle = `rgb( ${255 - 255 * this.pheromones}, ${255 * this.pheromones},  ${255 * this.pheromones})`;
+        context.font = "10px Arial";
+        context.fillText(this.pheromones.toString() ,(x * cellSize) + cellSize/2 , (y * cellSize) + cellSize/2);
     }
 
      display(canvas, x, y, cellSize) {
         const context = canvas.getContext('2d')
-        const grassImg = ImageLoader.instance.images[CellType.FLOOR]
-        const grassSquareSize = grassImg.img.width / grassImg.croppedValue
-        const grassXPos = grassImg.img.width * grassImg.xRatio
-        const grassYPos = grassImg.img.width * grassImg.yRatio
-        context.drawImage( grassImg.img, grassXPos, grassYPos, grassSquareSize, grassSquareSize, x * cellSize, y * cellSize, cellSize, cellSize);
+        this.draw_default_grass(context, x, y, cellSize)
 
         if (this.type != CellType.FLOOR){
-            const squareSize = this.image.img.width / this.image.croppedValue
-            const xPos = this.image.img.width * this.image.xRatio
-            const yPos = this.image.img.width * this.image.yRatio
-            const objectSize = cellSize * this.image.sizeRatio
-            context.drawImage( this.image.img, xPos, yPos, squareSize, squareSize, (x * cellSize) , (y * cellSize), objectSize, objectSize);
+            this.draw_image(context, x, y, cellSize)
+        }
+
+        if (this.type != CellType.OBSTACLE && this.type != CellType.TREE) {
+            this.draw_pheromone(context, x, y, cellSize)
         }
     }
 }
