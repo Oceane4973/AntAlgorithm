edc9d87c0e0557b50afb8fd2e20829f4dcdf2cd2
Océane MONGES <oceane@bebou.oceanemonges.com>
2024-01-26 08:43:41 +0100

    update assets

--- src/loader/ImageLoader.js
+++ src/loader/ImageLoader.js
@@ -10,7 +10,7 @@ class ImageLoader {
             ImageLoader.instance.images = {
                 FOOD : new ImageData("./src/resources/assets/foodAndColony.png",15, 0, 0.875, 0.8),
                 FLOOR : new ImageData("./src/resources/assets/grass.png",4 ),
-                ANTHILL: new ImageData("./src/resources/assets/cellules.png",5, 0.39, 0.85, 0.6),
+                ANTHILL: new ImageData("./src/resources/assets/cellules.png",5, 0.4, 0.73, 0.6),
                 OBSTACLE : new ImageData("./src/resources/assets/foodAndColony.png",15, 0, 1.06, 1),
                 TREE : new ImageData("./src/resources/assets/tree.png",4, 0.03, 0.04 ),
                 ANT : new ImageData("./src/resources/assets/ant.png", 1, 0, 0, 0.5)
