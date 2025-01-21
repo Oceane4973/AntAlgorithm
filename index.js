d00cf4c583ff5be78d01b975fce7dd2134fe2786
Océane MONGES <oceane@bebou.oceanemonges.com>
2025-01-21 11:28:00 +0100

    Updated .gitignore

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ import ControlPanel from "./src/controllers/ControlPanel.controller.js";
 import Canvas from './src/controllers/Canvas.controller.js'
 import GameEngine from "./src/GameEngine.js";
 
-
 window.addEventListener('load', async () => {
-
     const screenHeight = window.innerHeight
     const canvasHeight = screenHeight * 0.9
 
     const canvasController = new Canvas("game", canvasHeight, canvasHeight)
+    // Exposer le canvasController globalement pour les tests
+    window.canvasController = canvasController;
+    
     await canvasController.generateMap()
 
     const engine = new GameEngine(canvasController);
+    // Exposer le GameEngine globalement pour les tests
+    window.gameEngine = engine;
+    
     new ControlPanel("#control", "#pheromones", engine);
 })
 
